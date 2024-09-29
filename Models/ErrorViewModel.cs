c98b0c1e460b6d69a1cbae0696255cd8d853d496
eduardo0804 <jesuslazbra1008@outlook.com>
2024-09-28 20:12:22 -0500

    Creación inicial del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace apppractica_2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
